f47bbc759073b99cef687a0ea8f44c8d7f85497f
Islom Abdurakhmonov <islomabdurakhmonov8@gmail.com>
2023-09-06 16:41:34 +0500

    migrate fault

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
--- server/app/models.py
+++ server/app/models.py
@@ -1,8 +1,6 @@
 from datetime import datetime
-
-
 from app import db
-
+import enum
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -13,36 +11,45 @@ class User(db.Model):
     contact = db.Column(db.String(13), unique=True, nullable=False)
     image_file_url = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
-
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class PostField(db.Model):
     __tablename__ = 'postfield'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), unique=True)
-    
-    
+    title = db.Column(db.String(30))
 
 class Post(db.Model):
     __tablename__ = 'post'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.relationship("User", lazy=True)
-    field = db.relationship("PostField", lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    field_id = db.Column(db.Integer, db.ForeignKey('postfield.id'), nullable=False)
     title = db.Column(db.String(60), nullable=False)
-    description = db.Column(db.String(120), nullable=True)
-
+    description = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class PostImage(db.Model):
     __tablename__ = "postimage"
 
     id = db.Column(db.Integer, primary_key=True)
-    post_id = db.relationship("Post", lazy=True)
-    image_url = db.Column(db.String(60), unique=True, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    image_url = db.Column(db.String(60), nullable=False)
 
+class MessageType(enum.Enum):
+    MESSAGE = "message"
+    FILE = "file"
+    AUDIO = "audio"
 
-    
+class Message(db.Model):
+    __tablename__ = 'message'
 
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    question = db.Column(db.String(256), nullable=False)
+    answer = db.Column(db.String(256), nullable=False)
+    type = db.Column(db.Enum(MessageType), default=MessageType.MESSAGE)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 from app import app
 
