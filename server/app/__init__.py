690c381b72e2d4eb953398f676d6b526192e30e3
Arslonbek25 <amadyarov25@gmail.com>
2023-09-06 14:54:02 +0500

    Add db migrations

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,12 +1,13 @@
 from app.config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 from app.views import home
--- server/app/views/home.py
+++ server/app/views/home.py
@@ -1,5 +1,42 @@
-from app import app, models
+import os
+import secrets
+from http import HTTPStatus
+
+from flask import request
+
+from app import app
+
 
 @app.get("/")
 def home():
-    return "hidfsd"
+    return "hello"
+
+
+def allowed_file(filename):
+    return filename.split(".")[-1] in ("png", "jpg", "jpeg")
+
+
+def save_picture(file):
+    random_hex = secrets.token_hex(8)
+    filename = random_hex + os.path.splitext(file.filename)[1]
+    path = os.path.join(app.root_path, "static", "x-rays", filename)
+    file.save(path)
+    return filename
+
+
+@app.post("/upload_image")
+# @login_required
+def set_picture():
+    file = request.files.get("image")
+    if file and allowed_file(file.filename):
+        filename = save_picture(file)
+
+        # old = os.path.join(app.root_path, "static", current_user.image_file)
+        # if os.path.exists(old):
+        #     os.remove(old)
+
+        # current_user.image_file = filename
+        # db.session.commit()
+        return {"message": "File uploaded successfully"}
+
+    return {"error": "Invalid file"}, HTTPStatus.BAD_REQUEST
