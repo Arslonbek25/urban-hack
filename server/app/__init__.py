683bc9defc115b3c2b0951d725c7bc3653ddebe3
Islom Abdurakhmonov <islomabdurakhmonov8@gmail.com>
2023-09-06 17:15:03 +0500

    models finishe

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_migrate import Migrate
 
+from app.config import Config
+from app.extensions import db
+# from app.models import User
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
--- server/app/models.py
+++ server/app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import enum
 
 from app import db
 
@@ -14,31 +14,21 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
 
 
-class PostField(db.Model):
-    __tablename__ = "postfield"
+class MessageType(enum.Enum):
+    MESSAGE = 'message'
+    FILE = 'file'
+
+
+class Message(db.Model):
+    __tablename__ = 'message'
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), unique=True)
-    
-    
-
-class Post(db.Model):
-    __tablename__ = "post"
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.relationship("User", lazy=True)
-    field = db.relationship("PostField", lazy=True)
-    title = db.Column(db.String(60), nullable=False)
-    description = db.Column(db.String(120), nullable=True)
-
-
-class PostImage(db.Model):
-    __tablename__ = "postimage"
-    id = db.Column(db.Integer, primary_key=True)
-    post_id = db.relationship("Post", lazy=True)
-    image_url = db.Column(db.String(60), unique=True, nullable=False)
-
-
-    
-
+    user_id = db.relationship('User', lazy=True)
+    type = db.Column(db.Enum(MessageType), nullable=False)
+    file_url = db.Column(db.String(64), nullable=False) 
+    question = db.Column(db.String(256), nullable=True)
+    answer = db.Column(db.String(256), nullable=True)
+    sended_date = db.Column(db.DateTime, default=datetime.utcnow)
 
 from app import app
 
